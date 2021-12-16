95d8eb4a9d99cfd614a3baafe241c0aab6a04f68
kakins <kakins@users.noreply.github.com>
2021-12-16 15:40:29 -0600

    Update ValuesController.cs

--- BlazoryQueryBuilder.Api/Controllers/ValuesController.cs
+++ BlazoryQueryBuilder.Api/Controllers/ValuesController.cs
@@ -21,21 +21,6 @@ namespace BlazoryQueryBuilder.Api.Controllers
             _queryServiceFactory = queryServiceFactory;
         }
 
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
         public async Task<ActionResult<IEnumerable>> Post([FromBody] Predicate predicate)
         {
@@ -43,18 +28,6 @@ namespace BlazoryQueryBuilder.Api.Controllers
             IEnumerable data = await queryService.QueryData(predicate.LambdaExpression, predicate.SelectedProperties);
 
             return Ok(data);
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
