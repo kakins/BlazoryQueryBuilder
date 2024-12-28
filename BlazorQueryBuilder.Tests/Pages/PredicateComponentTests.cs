3bc7335b3ecbdc64f76b04a6320f956c108be7ae
Kris Akins <krisakins@protonmail.com>
2024-12-27 18:12:48 -0600

    Added predicate component tests

---
+++ BlazorQueryBuilder.Tests/Pages/PredicateComponentTests.cs
@@ -0,0 +1,134 @@
+﻿using BlazorQueryBuilder.Pages;
+using BlazoryQueryBuilder.Shared.Models;
+using BlazoryQueryBuilder.Shared.Services;
+using Bunit;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlazorQueryBuilder.Tests.Pages
+{
+    public class PredicateComponentTests : TestContext
+    {
+        public PredicateComponentTests()
+        {
+            Services.AddSingleton<PredicateFactory>();
+
+            Services.AddMudServices();
+
+            JSInterop.Mode = JSRuntimeMode.Loose;
+            JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
+
+            RenderComponent<MudPopoverProvider>();
+        }
+
+        [Theory]
+        [MemberData(nameof(RelationalPredicateTestData))]
+        public async Task Initialiazes_relational_predicate(Expression<Func<Address, bool>> lambdaExpression)
+        {
+            // Arrange
+            // Act
+            var component = RenderComponent<BlazorQueryBuilder.Pages.Predicate>(parameters =>
+            {
+                parameters
+                    .Add(p => p.Expression, lambdaExpression.Body as BinaryExpression)
+                    .Add(p => p.Parameter, lambdaExpression.Parameters[0])
+                    .Add(p => p.OnChange, _ => { });
+            });
+
+            // Assert
+            var logicalPredicate = component.FindComponent<RelationalPredicate>();
+            logicalPredicate.Instance.Binary.Should().Be(lambdaExpression.Body as BinaryExpression);
+            logicalPredicate.Instance.Parameter.Should().Be(lambdaExpression.Parameters[0]);
+        }
+
+        [Theory]
+        [MemberData(nameof(LogicalPredicateTestData))]
+        public async Task Initializes_logical_predicate(Expression<Func<Person, bool>> lambdaExpression)
+        {
+            // Arrange
+            // Act
+            var component = RenderComponent<BlazorQueryBuilder.Pages.Predicate>(parameters =>
+            {
+                parameters
+                    .Add(p => p.Expression, lambdaExpression.Body as BinaryExpression)
+                    .Add(p => p.Parameter, lambdaExpression.Parameters[0])
+                    .Add(p => p.OnChange, _ => { });
+            });
+
+            // Assert
+            var logicalPredicate = component.FindComponent<LogicalPredicate>();
+            logicalPredicate.Instance.Binary.Should().Be(lambdaExpression.Body as BinaryExpression);
+            logicalPredicate.Instance.Parameter.Should().Be(lambdaExpression.Parameters[0]);
+        }
+
+        [Fact]
+        public async Task Updates_parent_when_logical_predicate_changes()
+        {
+            // Arrange
+            Expression<Func<Person, bool>> lambdaExpression = person => person.PersonId == "1" && person.PersonId == "2";
+            bool updated = false;
+            var component = RenderComponent<BlazorQueryBuilder.Pages.Predicate>(parameters =>
+            {
+                parameters
+                    .Add(p => p.Expression, lambdaExpression.Body as BinaryExpression)
+                    .Add(p => p.Parameter, lambdaExpression.Parameters[0])
+                    .Add(p => p.OnChange, _ => { updated = true;  });
+            });
+
+            // Act
+            var logicalPredicate = component.FindComponent<LogicalPredicate>();
+            await logicalPredicate.InvokeAsync(() =>
+            {
+                logicalPredicate.Instance.OnChange.Invoke(lambdaExpression.Body as BinaryExpression);
+            });
+
+            // Assert
+            updated.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Updates_parent_when_relational_predicate_changes()
+        {
+            // Arrange
+            Expression<Func<Person, bool>> lambdaExpression = person => person.PersonId == "1";
+            bool updated = false;
+            var component = RenderComponent<BlazorQueryBuilder.Pages.Predicate>(parameters =>
+            {
+                parameters
+                    .Add(p => p.Expression, lambdaExpression.Body as BinaryExpression)
+                    .Add(p => p.Parameter, lambdaExpression.Parameters[0])
+                    .Add(p => p.OnChange, _ => { updated = true; });
+            });
+
+            // Act
+            var relationalPredicate = component.FindComponent<RelationalPredicate>();
+            await relationalPredicate.InvokeAsync(() =>
+            {
+                relationalPredicate.Instance.OnChange.Invoke(lambdaExpression.Body as BinaryExpression);
+            });
+
+            // Assert
+            updated.Should().BeTrue();
+        }
+
+        public static TheoryData<Expression<Func<Address, bool>>> RelationalPredicateTestData =>
+        [
+            person => person.AddressId == 1,
+            person => person.AddressId != 1,
+            person => person.AddressId > 1,
+            person => person.AddressId < 1
+        ];
+
+        public static TheoryData<Expression<Func<Person, bool>>> LogicalPredicateTestData =>
+        [
+            person => person.PersonId == "1" && person.PersonId == "2",
+            person => person.PersonId == "1" || person.PersonId == "2",
+        ];
+    }
+}
