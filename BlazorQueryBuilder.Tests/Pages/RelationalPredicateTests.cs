767ea4aa523c337b9d6b78d629892eccb9198974
Kris Akins <krisakins@protonmail.com>
2025-01-05 11:42:59 -0600

    Moved relation value tests to new class

--- BlazorQueryBuilder.Tests/Pages/RelationalPredicateTests.cs
+++ BlazorQueryBuilder.Tests/Pages/RelationalPredicateTests.cs
@@ -167,170 +167,6 @@ namespace BlazorQueryBuilder.Tests.Pages
         }
 
         [Fact]
-        public async Task Initializes_int_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.NumberOfChildren == 4);
-            var predicateExpression = lambdaExpression.Body;
-
-            // Act
-            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);
-
-            // Assert
-            var valueInput = component.FindInputByLabel<MudTextField<int>, int>("Value");
-            valueInput.Instance.Value.Should().Be(4);
-        }
-
-        [Fact]
-        public async Task Updates_right_operand_to_int_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.NumberOfChildren == 4);
-            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression);
-
-            var component = CreateComponent(
-                predicateExpression, 
-                lambdaExpression.Parameters[0], 
-                updatedExpression => { predicateExpression = updatedExpression as BinaryExpression; });
-
-            // Act
-            var valueInput = component.FindInputByLabel<MudTextField<int>, int>("Value");
-            await component.InvokeAsync(() =>
-            {
-                valueInput.Instance.SetText("5");
-            });
-
-            // Assert
-            var rightOperand = predicateExpression.Right as ConstantExpression;
-            rightOperand.Value.Should().Be(5);
-        }
-
-        [Fact]
-        public async Task Initializes_string_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.PersonId == "1");
-            var predicateExpression = lambdaExpression.Body;
-
-            // Act
-            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);
-
-            // Assert
-            var valueInput = component.FindInputByLabel<MudTextField<string>, string>("Value");
-            valueInput.Instance.Value.Should().Be("1");
-        }
-
-        [Fact]
-        public async Task Updates_right_operand_to_string_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.PersonId == "1");
-            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression);
-
-            var component = CreateComponent(
-                predicateExpression,
-                lambdaExpression.Parameters[0],
-                onChange: updatedExpression => { predicateExpression = updatedExpression as BinaryExpression; });
-
-            // Act
-            var valueInput = component.FindInputByLabel<MudTextField<string>, string>("Value");
-            await component.InvokeAsync(() =>
-            {
-                valueInput.Instance.SetText("2");
-            });
-
-            // Assert
-            var rightOperand = predicateExpression.Right as ConstantExpression;
-            rightOperand.Value.Should().Be("2");
-        }
-
-        [Fact]
-        public async Task Initializes_date_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.Created == DateTime.MinValue);
-            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression).ReplaceRight(Expression.Constant(DateTime.Now));
-
-            // Act
-            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);
-
-            // Assert
-            var valueInput = component
-                .FindComponents<MudDatePicker>()
-                .FirstOrDefault(p => p.Instance.Label == "Value"); ;
-            valueInput.Instance.Date.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
-        }
-
-        [Fact]
-        public async Task Updates_right_operand_to_date_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.Created == DateTime.MinValue);
-            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression).ReplaceRight(Expression.Constant(DateTime.Now));
-
-            var component = CreateComponent(
-                predicateExpression, 
-                lambdaExpression.Parameters[0],
-                onChange: updatedExpression => { predicateExpression = updatedExpression as BinaryExpression; });
-
-            // Act
-            var dateTime = DateTime.Now.AddDays(1);
-            var dateValueInput = component
-                .FindComponents<MudDatePicker>()
-                .FirstOrDefault(p => p.Instance.Label == "Value");
-            await component.InvokeAsync(async () =>
-            {
-                await dateValueInput.Instance.DateChanged.InvokeAsync(dateTime);
-            });
-
-            // Assert
-            var rightOperand = predicateExpression.Right as ConstantExpression;
-            rightOperand.Value.Should().Be(dateTime);
-        }
-
-        [Fact]
-        public async Task Initializes_bool_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.IsAlive == true);
-            var predicateExpression = lambdaExpression.Body;
-
-            // Act
-            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);   
-
-            // Assert
-            var valueInput = component.FindComponents<MudSelect<bool>>().FirstOrDefault(c => c.Instance.Label == "Value"); ;
-            valueInput.Instance.Value.Should().BeTrue();
-        }
-
-        [Fact]
-        public async Task Updates_right_operand_to_bool_value()
-        {
-            // Arrange
-            var lambdaExpression = GetLambdaExpression<Person>(person => person.IsAlive == true);
-            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression);
-
-            var component = CreateComponent(
-                predicateExpression, 
-                lambdaExpression.Parameters[0], 
-                onChange: updatedExpression => { predicateExpression = updatedExpression as BinaryExpression; });
-
-            // Act
-            var valueInput = component
-                .FindComponents<MudSelect<bool>>()
-                .FirstOrDefault(c => c.Instance.Label == "Value");
-            
-            await component.InvokeAsync(async () =>
-            {
-                await valueInput.Instance.ValueChanged.InvokeAsync(false);
-            });
-
-            // Assert
-            var rightOperand = predicateExpression.Right as ConstantExpression;
-            rightOperand.Value.Should().Be(false);
-        }
-
-        [Fact]
         public async Task Updates_operators_on_value_change()
         {
             // Arrange
@@ -340,16 +176,22 @@ namespace BlazorQueryBuilder.Tests.Pages
                 lambdaExpression.Parameters[0]);
 
             // Act
-            var valueInput = component.FindInputByLabel<MudTextField<string>, string>("Value");
+            var updatedExpression = lambdaExpression.Body
+                .As<BinaryExpression>()
+                .ReplaceRight(Expression.Constant("2"));
+            var value = component.FindComponent<RelationalValue>();
             await component.InvokeAsync(() =>
             {
-                valueInput.Instance.SetText("2");
+                value.Instance.OnValueChanged(updatedExpression);
             });
 
             // Assert
-            var updatedExpression = GetLambdaExpression<Person>(person => person.PersonId == "2").Body;
-            var operators = component.FindComponent<RelationalOperators>();
-            operators.Instance.PredicateExpression.Should().BeEquivalentTo(updatedExpression);
+            component
+                .FindComponent<RelationalOperators>()
+                .Instance
+                .PredicateExpression
+                .Should()
+                .BeEquivalentTo(updatedExpression);
         }
 
         [Theory]
---
+++ BlazorQueryBuilder.Tests/Pages/RelationalValueTests.cs
@@ -0,0 +1,227 @@
+﻿using BlazorQueryBuilder.ExpressionVisitors.Extensions;
+using BlazorQueryBuilder.Pages;
+using BlazorQueryBuilder.Tests.Util;
+using BlazoryQueryBuilder.Shared.Models;
+using BlazoryQueryBuilder.Shared.Services;
+using Bunit;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using MudBlazor;
+using MudBlazor.Services;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlazorQueryBuilder.Tests.Pages
+{
+    public class RelationalValueTests : TestContext
+    {
+        private readonly LambdaExpression _lambdaExpression;
+        private readonly ParameterExpression _predicateParameter;
+        private readonly Expression _predicateExpression;
+
+        public RelationalValueTests()
+        {
+            Services.AddSingleton<PredicateFactory>();
+
+            Services.AddMudServices();
+
+            JSInterop.Mode = JSRuntimeMode.Loose;
+            JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
+
+            RenderComponent<MudPopoverProvider>();
+
+            _lambdaExpression = GetLambdaExpression<Person>(person => person.PersonId == "1");
+            _predicateParameter = _lambdaExpression.Parameters[0];
+            _predicateExpression = _lambdaExpression.Body;
+        }
+
+        [Fact]
+        public async Task Initializes_int_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.NumberOfChildren == 4);
+            var predicateExpression = lambdaExpression.Body;
+
+            // Act
+            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);
+
+            // Assert
+            var valueInput = component.FindInputByLabel<MudTextField<int>, int>("Value");
+            valueInput.Instance.Value.Should().Be(4);
+        }
+
+        [Fact]
+        public async Task Updates_right_operand_to_int_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.NumberOfChildren == 4);
+            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression);
+            var onValueChanged = new Mock<Action<Expression>>();
+            
+            var component = CreateComponent(
+                predicateExpression,
+                lambdaExpression.Parameters[0],
+                onValueChanged.Object);
+
+            // Act
+            var valueInput = component.FindInputByLabel<MudTextField<int>, int>("Value");
+            await component.InvokeAsync(() =>
+            {
+                valueInput.Instance.SetText("5");
+            });
+
+            // Assert
+            onValueChanged.Verify(change => change(It.Is<BinaryExpression>(e => e.Right.As<ConstantExpression>().Value.As<int>() == 5)));
+        }
+
+        [Fact]
+        public async Task Initializes_string_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.PersonId == "1");
+            var predicateExpression = lambdaExpression.Body;
+
+            // Act
+            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);
+
+            // Assert
+            var valueInput = component.FindInputByLabel<MudTextField<string>, string>("Value");
+            valueInput.Instance.Value.Should().Be("1");
+        }
+
+        [Fact]
+        public async Task Updates_right_operand_to_string_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.PersonId == "1");
+            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression);
+            var onValueChanged = new Mock<Action<Expression>>();
+
+            var component = CreateComponent(
+                predicateExpression,
+                lambdaExpression.Parameters[0],
+                onValueChanged: onValueChanged.Object);
+
+            // Act
+            var valueInput = component.FindInputByLabel<MudTextField<string>, string>("Value");
+            await component.InvokeAsync(() =>
+            {
+                valueInput.Instance.SetText("2");
+            });
+
+            // Assert
+            onValueChanged.Verify(change => change(It.Is<BinaryExpression>(e => e.Right.As<ConstantExpression>().Value.As<string>() == "2")));
+        }
+
+        [Fact]
+        public async Task Initializes_date_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.Created == System.DateTime.MinValue);
+            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression).ReplaceRight(Expression.Constant(System.DateTime.Now));
+
+            // Act
+            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);
+
+            // Assert
+            var valueInput = component
+                .FindComponents<MudDatePicker>()
+                .FirstOrDefault(p => p.Instance.Label == "Value"); ;
+            valueInput.Instance.Date.Should().BeCloseTo(System.DateTime.Now, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public async Task Updates_right_operand_to_date_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.Created == System.DateTime.MinValue);
+            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression).ReplaceRight(Expression.Constant(System.DateTime.Now));
+            var onValueChanged = new Mock<Action<Expression>>();
+
+            var component = CreateComponent(
+                predicateExpression,
+                lambdaExpression.Parameters[0],
+                onValueChanged: onValueChanged.Object);
+
+            // Act
+            var dateTime = System.DateTime.Now.AddDays(1);
+            var dateValueInput = component
+                .FindComponents<MudDatePicker>()
+                .FirstOrDefault(p => p.Instance.Label == "Value");
+            await component.InvokeAsync(async () =>
+            {
+                await dateValueInput.Instance.DateChanged.InvokeAsync(dateTime);
+            });
+
+            // Assert
+            onValueChanged.Verify(change => change(It.Is<BinaryExpression>(e => e.Right.As<ConstantExpression>().Value.As<System.DateTime>() == dateTime)));
+        }
+
+        [Fact]
+        public async Task Initializes_bool_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.IsAlive == true);
+            var predicateExpression = lambdaExpression.Body;
+
+            // Act
+            var component = CreateComponent(predicateExpression, lambdaExpression.Parameters[0]);
+
+            // Assert
+            var valueInput = component.FindComponents<MudSelect<bool>>().FirstOrDefault(c => c.Instance.Label == "Value"); ;
+            valueInput.Instance.Value.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Updates_right_operand_to_bool_value()
+        {
+            // Arrange
+            var lambdaExpression = GetLambdaExpression<Person>(person => person.IsAlive == true);
+            var predicateExpression = GetLambdaBodyExpression<BinaryExpression>(lambdaExpression);
+            var onValueChanged = new Mock<Action<Expression>>();
+
+            var component = CreateComponent(
+                predicateExpression,
+                lambdaExpression.Parameters[0],
+                onValueChanged: onValueChanged.Object);
+
+            // Act
+            var valueInput = component
+                .FindComponents<MudSelect<bool>>()
+                .FirstOrDefault(c => c.Instance.Label == "Value");
+
+            await component.InvokeAsync(async () =>
+            {
+                await valueInput.Instance.ValueChanged.InvokeAsync(false);
+            });
+
+            // Assert
+            onValueChanged.Verify(change => change(It.Is<BinaryExpression>(e => e.Right.As<ConstantExpression>().Value.As<bool>() == false)));
+        }
+
+        private IRenderedComponent<RelationalValue> CreateComponent(
+            Expression predicateExpression = null,
+            ParameterExpression parameterExpression = null,
+            Action<Expression> onValueChanged = null)
+        {
+            onValueChanged ??= (_) => { };
+
+            return RenderComponent<RelationalValue>(parameters =>
+            {
+                parameters
+                    .Add(p => p.PredicateExpression, predicateExpression ?? _predicateExpression)
+                    .Add(p => p.ParameterExpression, parameterExpression ?? _predicateParameter)
+                    .Add(p => p.OnValueChanged, onValueChanged);
+            });
+        }
+
+        private LambdaExpression GetLambdaExpression<T>(Expression<Func<T, bool>> lambdaExpression) => lambdaExpression;
+        
+        private T GetLambdaBodyExpression<T>(LambdaExpression lambdaExpression) where T : Expression => lambdaExpression.Body as T;
+
+    }
+}
